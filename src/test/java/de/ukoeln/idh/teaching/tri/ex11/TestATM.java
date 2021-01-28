f57e69f9e2a5603e80c3995042ae2953cc9ae10d
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2021-01-28 13:22:22 +0100

    added dummy test

--- src/test/java/de/ukoeln/idh/teaching/tri/ex11/TestATM.java
+++ src/test/java/de/ukoeln/idh/teaching/tri/ex11/TestATM.java
@@ -1,5 +1,14 @@
 package de.ukoeln.idh.teaching.tri.ex11;
 
-public class TestATM {
+import static org.junit.jupiter.api.Assertions.fail;
 
+import org.junit.jupiter.api.Test;
+
+public class TestATM {
+	@Test
+	public void testTest() {
+
+		// a dummy test that always fails
+		fail();
+	}
 }
