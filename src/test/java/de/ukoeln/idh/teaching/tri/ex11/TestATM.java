f87c35f5daf17adf6f15e39acecfab0248d827c1
wolfandreh <wolf-andre_hummel@live.de>
2021-02-03 15:01:21 +0100

    Finished exercise by writing 2 simple test-functions.

--- src/test/java/de/ukoeln/idh/teaching/tri/ex11/TestATM.java
+++ src/test/java/de/ukoeln/idh/teaching/tri/ex11/TestATM.java
@@ -1,14 +1,36 @@
 package de.ukoeln.idh.teaching.tri.ex11;
 
-import static org.junit.jupiter.api.Assertions.fail;
-
 import org.junit.jupiter.api.Test;
 
-public class TestATM {
-	@Test
-	public void testTest() {
+import static org.junit.jupiter.api.Assertions.*;
 
-		// a dummy test that always fails
-		fail();
+public class TestATM {
+
+	private ATM atm = new ATM();
+
+	@Test
+	public void CombinationTest() {
+
+		assertArrayEquals(new int[] {1,0,0,0,0,0,0}, atm.withdraw(500));
+
+		assertArrayEquals(new int[] {0,0,0,0,0,0,1}, atm.withdraw(5));
+
+		assertArrayEquals(new int[] {1,0,0,0,0,0,1}, atm.withdraw(505));
+
+		assertArrayEquals(new int[] {1,0,0,1,0,0,1}, atm.withdraw(555));
+
+	}
+
+	@Test
+	public void FailTest() {
+
+		assertEquals(false, atm.canWithdraw(0));
+
+		assertEquals(false, atm.canWithdraw(-500));
+
+		assertEquals(false, atm.canWithdraw(Integer.MAX_VALUE));
+
+		assertEquals(false, atm.canWithdraw(Integer.MIN_VALUE));
+
 	}
 }
