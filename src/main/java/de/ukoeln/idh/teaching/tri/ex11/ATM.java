03b3bf220fc6c70fc9fc601721ecb737aa8930fc
Kerstin <kerstin.vankan@web.de>
2021-02-03 11:07:47 +0100

    TestATM und ATM > Update

--- src/main/java/de/ukoeln/idh/teaching/tri/ex11/ATM.java
+++ src/main/java/de/ukoeln/idh/teaching/tri/ex11/ATM.java
@@ -18,61 +18,67 @@ public class ATM {
 	 * @return An array of integers, showing how many of which bill to return (in
 	 *         descending order)
 	 */
-	public int[] withdraw(int amount) {
-		int[] numberOfBills = new int[bills.length];
+
+
+public int[] withdraw(int amount) {
+	int[] numberOfBills = new int[bills.length];
+	if(amount > 0){
 		for (int b = 0; b < bills.length; b++) {
 			int currentBill = bills[b];
 			numberOfBills[b] = amount / currentBill;
 			amount = amount - (numberOfBills[b] * currentBill);
 		}
-		if (amount == 0) {
-			return numberOfBills;
-		} else {
-			throw new RuntimeException("Cannot withdraw");
-		}
-
 	}
 
-	protected boolean canWithdraw(int value) {
-		return value % 5 == 0 && value > 0;
+	if (amount == 0) {
+		return numberOfBills;
+	} else {
+		throw new RuntimeException("Cannot withdraw");
 	}
 
-	/**
-	 * Main user loop. Allows users to enter an integer number or "exit" to leave
-	 * the loop.
-	 */
-	public void run() {
-		String userChoice;
-		System.out.print("Please enter amount: ");
-		try (Scanner in = new Scanner(System.in)) {
-			do {
-				userChoice = in.next();
-				if (userChoice.equalsIgnoreCase("exit")) {
-					break;
-				} else if (userChoice.matches("^\\d+$")) {
-					int userValue = Integer.valueOf(userChoice);
-					try {
-						if (canWithdraw(userValue)) {
-							int[] bills = withdraw(userValue);
-							System.out.println(StringUtils.join(bills, ','));
-						} else {
-							System.out.println("Incorrect value");
-						}
-					} catch (RuntimeException e) {
+}
+
+protected boolean canWithdraw(int value) {
+	return value % 5 == 0 && value > 0;
+}
+
+/**
+ * Main user loop. Allows users to enter an integer number or "exit" to leave
+ * the loop.
+ */
+public void run() {
+	String userChoice;
+	System.out.print("Please enter amount: ");
+	try (Scanner in = new Scanner(System.in)) {
+		do {
+			userChoice = in.next();
+			if (userChoice.equalsIgnoreCase("exit")) {
+				break;
+			} else if (userChoice.matches("^\\d+$")) {
+				int userValue = Integer.parseInt(userChoice);
+				try {
+					if (canWithdraw(userValue)) {
+						int[] bills = withdraw(userValue);
+						System.out.println(StringUtils.join(bills, ','));
+					} else {
 						System.out.println("Incorrect value");
 					}
-				} else {
+				} catch (RuntimeException e) {
 					System.out.println("Incorrect value");
 				}
-				System.out.println();
-				System.out.print("Please enter amount: ");
-			} while (in.hasNext());
-		}
+			} else {
+				System.out.println("Incorrect value");
+			}
+			System.out.println();
+			System.out.print("Please enter amount: ");
+		} while (in.hasNext());
 	}
+}
 
-	public static void main(String[] args) {
-		// create a new instance and launch it
-		new ATM().run();
-	}
+public static void main(String[] args) {
+	// create a new instance and launch it
+	new ATM().run();
+
+}
 
 }
--- src/test/java/de/ukoeln/idh/teaching/tri/ex11/TestATM.java
+++ src/test/java/de/ukoeln/idh/teaching/tri/ex11/TestATM.java
@@ -1,5 +1,36 @@
 package de.ukoeln.idh.teaching.tri.ex11;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
 public class TestATM {
 
+	ATM atm = new ATM();
+	
+	@Test
+	void testInput() {
+		assertEquals(true, atm.canWithdraw(5));
+		assertEquals(false, atm.canWithdraw(0));
+		assertEquals(false, atm.canWithdraw(-5));
+		assertEquals(true, atm.canWithdraw(505));
+		assertEquals(true, atm.canWithdraw(2147483645));
+		assertEquals(false, atm.canWithdraw(Integer.MAX_VALUE));
+		assertEquals(false, atm.canWithdraw(Integer.MIN_VALUE));
+		assertEquals(false, atm.canWithdraw(-2147483645));
+		
+		//{ 500, 200, 100, 50, 20, 10, 5 }
+		assertArrayEquals(new int[] {1,0,0,0,0,0,0}, atm.withdraw(500));
+		assertArrayEquals(new int[] {0,1,0,0,0,0,0}, atm.withdraw(200));
+		assertArrayEquals(new int[] {1,0,1,0,0,0,0}, atm.withdraw(600));
+		assertArrayEquals(new int[] {1,0,0,1,0,0,0}, atm.withdraw(550));
+		assertArrayEquals(new int[] {1,0,0,0,1,0,0}, atm.withdraw(520));
+		assertArrayEquals(new int[] {0,0,0,0,0,1,1}, atm.withdraw(15));
+		assertArrayEquals(new int[] {0,0,0,0,0,0,1}, atm.withdraw(5));
+		
+		assertThrows(RuntimeException.class, ()->{atm.withdraw(521);});
+		assertThrows(RuntimeException.class, ()->{atm.withdraw(-10);});
+	}
 }
