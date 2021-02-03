e919c3eafa0f4e701f416a165a5a6c71572824f0
Corinna Munsch <cmunsch@smail.uni-koeln.de>
2021-02-03 20:26:58 +0100

    changed TestATM
    someone already pushed their version to the main branch, but I tried my hand anyways :-D

--- src/test/java/de/ukoeln/idh/teaching/tri/ex11/TestATM.java
+++ src/test/java/de/ukoeln/idh/teaching/tri/ex11/TestATM.java
@@ -1,8 +1,9 @@
 package de.ukoeln.idh.teaching.tri.ex11;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
@@ -10,26 +11,26 @@ public class TestATM {
 
 	ATM atm = new ATM();
 	
-	public void testWithdraw() {
-		assertArrayEquals(new int[] {0, 0, 0, 0, 0, 0, 1 }, atm.withdraw(5));
-		assertArrayEquals(new int[] {0, 0, 0, 0, 0, 1, 1 }, atm.withdraw(15));
-		assertArrayEquals(new int[] {0, 0, 0, 0, 1, 0, 1 }, atm.withdraw(25));
-		assertArrayEquals(new int[] {0, 0, 0, 1, 0, 0, 1 }, atm.withdraw(55));
-		assertArrayEquals(new int[] {0, 0, 1, 0, 0, 0, 1 }, atm.withdraw(105));
-		assertArrayEquals(new int[] {0, 1, 0, 0, 0, 0, 1 }, atm.withdraw(205));
-		assertArrayEquals(new int[] {1, 0, 0, 0, 0, 0, 1 }, atm.withdraw(505));
-		assertThrows(RuntimeException.class, ()->{atm.withdraw(321);});
-		assertThrows(RuntimeException.class, ()->{atm.withdraw(-20);});
-	}
+	/**
+	 * This test compares the banknote arrays to the selected amount.
+	 */
 	@Test
-	public void testCanWithdraw() {
-		assertTrue(atm.canWithdraw(25));
-		assertFalse(atm.canWithdraw(-25));
-		assertTrue(atm.canWithdraw(205));
-		assertFalse(atm.canWithdraw(44));
-		assertTrue(atm.canWithdraw(300));
-		assertFalse(atm.canWithdraw(79));
-		assertTrue(atm.canWithdraw(55));
-		assertFalse(atm.canWithdraw(0));
+	public void testWithdraw() {
+		assertArrayEquals(new int[] {0, 0, 0, 0, 2, 0, 1 }, atm.withdraw(45));
+		assertArrayEquals(new int[] {0, 1, 1, 0, 0, 1, 0 }, atm.withdraw(310));
+		assertArrayEquals(new int[] {1, 0, 0, 0, 1, 0, 1 }, atm.withdraw(525));
+		assertArrayEquals(new int[] {2, 0, 0, 1, 0, 0, 0 }, atm.withdraw(1050));
+		assertThrows(RuntimeException.class, ()->{atm.withdraw(1584);});
+		assertThrows(RuntimeException.class, ()->{atm.withdraw(-100);});
+	}
+	/**
+	 * This test @param value asserts if it is possible to Withdraw the value given, the maximal and minimal value.
+	 */
+	@Test
+	public void testCanWithdraw(int value) {
+		assertTrue(atm.canWithdraw(value));
+		assertFalse(atm.canWithdraw(-value));
+		assertFalse(atm.canWithdraw(Integer.MAX_VALUE));
+		assertFalse(atm.canWithdraw(Integer.MIN_VALUE));
 	}
 }
