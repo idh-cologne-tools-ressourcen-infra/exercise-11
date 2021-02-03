db72d7110618947957e1911998ec2af94bfde8a6
Nico <w>
2021-02-03 19:06:44 +0100

    exercise11

--- src/main/java/de/ukoeln/idh/teaching/tri/ex11/ATM.java
+++ src/main/java/de/ukoeln/idh/teaching/tri/ex11/ATM.java
@@ -1,14 +1,54 @@
 package de.ukoeln.idh.teaching.tri.ex11;
-
-import static org.junit.jupiter.api.Assertions.fail;
-
-import org.junit.jupiter.api.Test;
-
-public class TestATM {
-	@Test
-	public void testTest() {
-
-		// a dummy test that always fails
-		fail();
+import java.util.Scanner;
+import org.apache.commons.lang3.StringUtils;
+public class ATM {
+	/**
+	 * The bills available in the currency system (in this case: Euros)
+	 */
+	int[] bills = new int[] { 500, 200, 100, 50, 20, 10, 5 };
+	/**
+	 * Withdrawing logic.
+	 * 
+	 * @param amount The amount to withdraw
+	 * @return An array of integers, showing how many of which bill to return (in
+	 *         descending order)
+	 */
+	public int[] withdraw(int amount) {
+		int[] numberOfBills = new int[bills.length];
+		for (int b = 0; b < bills.length; b++) {
+			int currentBill = bills[b];
+			numberOfBills[b] = amount / currentBill;
+			amount = amount - (numberOfBills[b] * currentBill);
+		}
+		if (amount == 0) {
+			return numberOfBills;
+		} else {
+			throw new RuntimeException("Cannot withdraw");
+		}
 	}
+	protected boolean canWithdraw(int value) {
+		return value % 5 == 0 && value > 0;
+	}
+	/**
+	 * Main user loop. Allows users to enter an integer number or "exit" to leave
+	 * the loop.
+	 */
+	public void run() {
+		String userChoice;
+		System.out.print("Please enter amount: ");
+		try (Scanner in = new Scanner(System.in)) {
+			do {
+				userChoice = in.next();
+				if (userChoice.equalsIgnoreCase("exit")) {
+					break;
+				} else if (userChoice.matches("^\\d+$")) {
+				} else if (userChoice.matches("^\\d{1,10}$")) {
+					int userValue = Integer.valueOf(userChoice);
+					try {
+						if (canWithdraw(userValue)) {
+@@ -75,4 +75,4 @@ public static void main(String[] args) {
+		new ATM().run();
+	}
+
 }
+} 
--- src/test/java/de/ukoeln/idh/teaching/tri/ex11/TestATM.java
+++ src/test/java/de/ukoeln/idh/teaching/tri/ex11/TestATM.java
@@ -1,14 +1,35 @@
 package de.ukoeln.idh.teaching.tri.ex11;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
 public class TestATM {
-	@Test
-	public void testTest() {
 
-		// a dummy test that always fails
-		fail();
+	ATM atm = new ATM();
+
+	public void testWithdraw() {
+		assertArrayEquals(new int[] {0, 0, 0, 0, 0, 0, 1 }, atm.withdraw(5));
+		assertArrayEquals(new int[] {0, 0, 0, 0, 0, 1, 1 }, atm.withdraw(15));
+		assertArrayEquals(new int[] {0, 0, 0, 0, 1, 0, 1 }, atm.withdraw(25));
+		assertArrayEquals(new int[] {0, 0, 0, 1, 0, 0, 1 }, atm.withdraw(55));
+		assertArrayEquals(new int[] {0, 0, 1, 0, 0, 0, 1 }, atm.withdraw(105));
+		assertArrayEquals(new int[] {0, 1, 0, 0, 0, 0, 1 }, atm.withdraw(205));
+		assertArrayEquals(new int[] {1, 0, 0, 0, 0, 0, 1 }, atm.withdraw(505));
+		assertThrows(RuntimeException.class, ()->{atm.withdraw(321);});
+		assertThrows(RuntimeException.class, ()->{atm.withdraw(-20);});
+	}
+
+	public void testCanWithdraw() {
+		assertTrue(atm.canWithdraw(25));
+		assertFalse(atm.canWithdraw(-25));
+		assertTrue(atm.canWithdraw(205));
+		assertFalse(atm.canWithdraw(44));
+		assertTrue(atm.canWithdraw(300));
+		assertFalse(atm.canWithdraw(79));
+		assertTrue(atm.canWithdraw(55));
+		assertFalse(atm.canWithdraw(0));
 	}
 }
