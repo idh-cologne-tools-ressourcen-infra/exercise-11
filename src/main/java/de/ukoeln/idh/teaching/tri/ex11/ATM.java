63cc79a763ef7b3760b1b2e06b181688de164969
Nico <w>
2021-02-03 19:00:19 +0100

    großes sorry! Ausversehen auf Main gepushed
    auf Aufgabenstellung zurückgesetzt

--- src/main/java/de/ukoeln/idh/teaching/tri/ex11/ATM.java
+++ src/main/java/de/ukoeln/idh/teaching/tri/ex11/ATM.java
@@ -1,78 +1,14 @@
 package de.ukoeln.idh.teaching.tri.ex11;
 
-import java.util.Scanner;
+import static org.junit.jupiter.api.Assertions.fail;
 
-import org.apache.commons.lang3.StringUtils;
+import org.junit.jupiter.api.Test;
 
-public class ATM {
+public class TestATM {
+	@Test
+	public void testTest() {
 
-	/**
-	 * The bills available in the currency system (in this case: Euros)
-	 */
-	int[] bills = new int[] { 500, 200, 100, 50, 20, 10, 5 };
-
-	/**
-	 * Withdrawing logic.
-	 * 
-	 * @param amount The amount to withdraw
-	 * @return An array of integers, showing how many of which bill to return (in
-	 *         descending order)
-	 */
-	public int[] withdraw(int amount) {
-		int[] numberOfBills = new int[bills.length];
-		for (int b = 0; b < bills.length; b++) {
-			int currentBill = bills[b];
-			numberOfBills[b] = amount / currentBill;
-			amount = amount - (numberOfBills[b] * currentBill);
-		}
-		if (amount == 0) {
-			return numberOfBills;
-		} else {
-			throw new RuntimeException("Cannot withdraw");
-		}
-
+		// a dummy test that always fails
+		fail();
 	}
-
-	protected boolean canWithdraw(int value) {
-		return value % 5 == 0 && value > 0;
-	}
-
-	/**
-	 * Main user loop. Allows users to enter an integer number or "exit" to leave
-	 * the loop.
-	 */
-	public void run() {
-		String userChoice;
-		System.out.print("Please enter amount: ");
-		try (Scanner in = new Scanner(System.in)) {
-			do {
-				userChoice = in.next();
-				if (userChoice.equalsIgnoreCase("exit")) {
-					break;
-				} else if (userChoice.matches("^\\d{1,10}$")) {
-					int userValue = Integer.valueOf(userChoice);
-					try {
-						if (canWithdraw(userValue)) {
-							int[] bills = withdraw(userValue);
-							System.out.println(StringUtils.join(bills, ','));
-						} else {
-							System.out.println("Incorrect value");
-						}
-					} catch (RuntimeException e) {
-						System.out.println("Incorrect value");
-					}
-				} else {
-					System.out.println("Incorrect value");
-				}
-				System.out.println();
-				System.out.print("Please enter amount: ");
-			} while (in.hasNext());
-		}
-	}
-
-	public static void main(String[] args) {
-		// create a new instance and launch it
-		new ATM().run();
-	}
-
 }
