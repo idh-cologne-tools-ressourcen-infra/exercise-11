d4f854752f0ccaf077a1e716f3fa3a88bb16bc7e
Nico <w>
2021-02-03 16:51:15 +0100

    exercise11 JUnit Test

--- src/test/java/de/ukoeln/idh/teaching/tri/ex11/TestATM.java
+++ src/test/java/de/ukoeln/idh/teaching/tri/ex11/TestATM.java
@@ -10,27 +10,26 @@ public class TestATM {
 
 	ATM atm = new ATM();
 	
+	public void testWithdraw() {
+		assertArrayEquals(new int[] {0, 0, 0, 0, 0, 0, 1 }, atm.withdraw(5));
+		assertArrayEquals(new int[] {0, 0, 0, 0, 0, 1, 1 }, atm.withdraw(15));
+		assertArrayEquals(new int[] {0, 0, 0, 0, 1, 0, 1 }, atm.withdraw(25));
+		assertArrayEquals(new int[] {0, 0, 0, 1, 0, 0, 1 }, atm.withdraw(55));
+		assertArrayEquals(new int[] {0, 0, 1, 0, 0, 0, 1 }, atm.withdraw(105));
+		assertArrayEquals(new int[] {0, 1, 0, 0, 0, 0, 1 }, atm.withdraw(205));
+		assertArrayEquals(new int[] {1, 0, 0, 0, 0, 0, 1 }, atm.withdraw(505));
+		assertThrows(RuntimeException.class, ()->{atm.withdraw(321);});
+		assertThrows(RuntimeException.class, ()->{atm.withdraw(-20);});
+	}
 	@Test
-	void testInput() {
-		assertEquals(true, atm.canWithdraw(5));
-		assertEquals(false, atm.canWithdraw(0));
-		assertEquals(false, atm.canWithdraw(-5));
-		assertEquals(true, atm.canWithdraw(505));
-		assertEquals(true, atm.canWithdraw(2147483645));
-		assertEquals(false, atm.canWithdraw(Integer.MAX_VALUE));
-		assertEquals(false, atm.canWithdraw(Integer.MIN_VALUE));
-		assertEquals(false, atm.canWithdraw(-2147483645));
-		
-		//{ 500, 200, 100, 50, 20, 10, 5 }
-		assertArrayEquals(new int[] {1,0,0,0,0,0,0}, atm.withdraw(500));
-		assertArrayEquals(new int[] {0,1,0,0,0,0,0}, atm.withdraw(200));
-		assertArrayEquals(new int[] {1,0,1,0,0,0,0}, atm.withdraw(600));
-		assertArrayEquals(new int[] {1,0,0,1,0,0,0}, atm.withdraw(550));
-		assertArrayEquals(new int[] {1,0,0,0,1,0,0}, atm.withdraw(520));
-		assertArrayEquals(new int[] {0,0,0,0,0,1,1}, atm.withdraw(15));
-		assertArrayEquals(new int[] {0,0,0,0,0,0,1}, atm.withdraw(5));
-		
-		assertThrows(RuntimeException.class, ()->{atm.withdraw(521);});
-		assertThrows(RuntimeException.class, ()->{atm.withdraw(-10);});
+	public void testCanWithdraw() {
+		assertTrue(atm.canWithdraw(25));
+		assertFalse(atm.canWithdraw(-25));
+		assertTrue(atm.canWithdraw(205));
+		assertFalse(atm.canWithdraw(44));
+		assertTrue(atm.canWithdraw(300));
+		assertFalse(atm.canWithdraw(79));
+		assertTrue(atm.canWithdraw(55));
+		assertFalse(atm.canWithdraw(0));
 	}
 }
