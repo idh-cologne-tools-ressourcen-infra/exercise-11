21d7c56b16c35e309efaeb1664d128a279b888ef
David <cruzzader@gmail.com>
2021-02-04 01:41:26 +0100

    Added unittests
    Added unittests

--- src/test/java/de/ukoeln/idh/teaching/tri/ex11/TestATM.java
+++ src/test/java/de/ukoeln/idh/teaching/tri/ex11/TestATM.java
@@ -1,14 +1,36 @@
 package de.ukoeln.idh.teaching.tri.ex11;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
 public class TestATM {
+	
+	ATM atm = new ATM();
+			
+			public void testWithdraw() {
+		assertArrayEquals(new int[] {0,0,0,0,0,0,1}, atm.withdraw(5));
+		assertArrayEquals(new int[] {0,0,0,0,0,1,1}, atm.withdraw(15));
+		assertArrayEquals(new int[] {0,0,0,0,1,0,1}, atm.withdraw(25));
+		assertArrayEquals(new int[] {0,0,0,1,0,0,1}, atm.withdraw(55));
+		assertArrayEquals(new int[] {0,0,1,0,0,0,1}, atm.withdraw(105));
+		assertArrayEquals(new int[] {0,1,0,0,0,0,1}, atm.withdraw(205));
+		assertArrayEquals(new int[] {1,0,0,0,0,0,1}, atm.withdraw(505));
+		assertThrows(RuntimeException.class, ()->{atm.withdraw(111);});
+		
+	}
 	@Test
-	public void testTest() {
-
-		// a dummy test that always fails
-		fail();
+	public void testCanWithdraw() {
+		assertTrue(atm.canWithdraw(25));
+		assertFalse(atm.canWithdraw(-25));
+		assertTrue(atm.canWithdraw(205));
+		assertFalse(atm.canWithdraw(44));
+		assertTrue(atm.canWithdraw(300));
+		assertFalse(atm.canWithdraw(Integer.MAX_VALUE));
+		assertTrue(atm.canWithdraw(Integer.MIN_VALUE));
+		assertFalse(atm.canWithdraw(0));	
 	}
 }
