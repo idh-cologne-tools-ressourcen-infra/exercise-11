2ef16b1e79bfb0f59af72ae1647e72df70be273b
bjenne <bjenne@smail.uni-koeln.de>
2021-02-11 01:57:53 +0100

    ATM

--- src/test/java/de/ukoeln/idh/teaching/tri/ex11/TestATM.java
+++ src/test/java/de/ukoeln/idh/teaching/tri/ex11/TestATM.java
@@ -1,14 +1,36 @@
 package de.ukoeln.idh.teaching.tri.ex11;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
 public class TestATM {
-	@Test
-	public void testTest() {
 
-		// a dummy test that always fails
-		fail();
+	ATM atm = new ATM();
+	
+	/**
+	 * This test compares the banknote arrays to the selected amount.
+	 */
+	@Test
+	public void testWithdraw() {
+		assertArrayEquals(new int[] {0, 0, 0, 0, 2, 0, 1 }, atm.withdraw(45));
+		assertArrayEquals(new int[] {0, 1, 1, 0, 0, 1, 0 }, atm.withdraw(310));
+		assertArrayEquals(new int[] {1, 0, 0, 0, 1, 0, 1 }, atm.withdraw(525));
+		assertArrayEquals(new int[] {2, 0, 0, 1, 0, 0, 0 }, atm.withdraw(1050));
+		assertThrows(RuntimeException.class, ()->{atm.withdraw(1584);});
+		assertThrows(RuntimeException.class, ()->{atm.withdraw(-100);});
+	}
+	/**
+	 * This test @param value asserts if it is possible to Withdraw the value given, the maximal and minimal value.
+	 */
+	@Test
+	public void testCanWithdraw(int value) {
+		assertTrue(atm.canWithdraw(value));
+		assertFalse(atm.canWithdraw(-value));
+		assertFalse(atm.canWithdraw(Integer.MAX_VALUE));
+		assertFalse(atm.canWithdraw(Integer.MIN_VALUE));
 	}
 }
